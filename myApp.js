ec58e304fe5c20c69c71eae64772d26edca24a47
Saulo de sousa Joseph <saulos.joseph@gmail.com>
2019-05-21 10:04:02 -0300

    exercise 2

--- myApp.js
+++ myApp.js
@@ -13,7 +13,9 @@ console.log('Hello World')
 
 
 /** 2) A first working Express Server */
-app.get((req, res) => res.send('Hello Express'));
+app.get('/', (req, res) => {
+    return res.send('Hello Express');
+});
 
 
 /** 3) Serve an HTML file */
