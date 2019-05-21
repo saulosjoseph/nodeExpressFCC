a1fe5180b2e17f44553bd2c03eea94960a37ffee
Saulo de sousa Joseph <saulos.joseph@gmail.com>
2019-05-21 16:33:07 -0300

    exercise 8

--- myApp.js
+++ myApp.js
@@ -69,7 +69,14 @@ app.get('/:word/echo', (req, res) => {
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
+app.get('/name', (req, res) => {
+    let firstname = req.query.first;
+    let lastname = req.query.last;
+    let echo =  req.params.word;
+    return res.json({
+        'name': `${firstname} ${lastname}`
+    })
+});
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
